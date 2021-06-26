4783ce1588dac9c6c1c66ae794bdbb9c5f30138f
Andre Borges <andreborges1777@gmail.com>
2021-06-26 20:59:03 -0300

    Adicionando instância do axios

---
+++ src/services/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import md5 from 'md5';
+
+const standardOptions = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  baseURL: 'http://gateway.marvel.com/v1/public',
+};
+
+const api = axios.create(standardOptions);
+
+api.interceptors.request.use((config) => {
+  const privateKey = '0429d9504bcaeca2d89cb63a6018a3d142dcf676';
+  const publicKey = '0429d9504bcaeca2d89cb63a6018a3d142dcf676';
+  const timeStamp = Number(new Date());
+  const hash = md5(`${timeStamp}${privateKey}${publicKey}`);
+
+  const configuration = {
+    ...config,
+    url: `${config.url}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
+  };
+
+  return configuration;
+});
+
+export default api;
